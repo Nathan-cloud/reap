8258be2b769c581b1f8324c2b7be3a45d31fb23f
Nathan-cloud <nathanmorgan867@gmail.com>
2020-09-28 23:13:19 -0700

    first commit

---
+++ assets/js/gal-js/main.js
@@ -0,0 +1,14 @@
+/** @format */
+
+function main() {
+	(function () {
+		'use strict';
+
+		// Nivo Lightbox
+		$('.portfolio-item a').nivoLightbox({
+			effect: 'slideDown',
+			keyboardNav: true,
+		});
+	})();
+}
+main();
